28cce93ccf40cb8b1770f7f26e41a698b52d05ac
Jacob Bearce <jacob@bearce.me>
2016-01-12 08:50:43 -0600

    Learnyounode challenge 4 (filtered ls)

--- program.js
+++ program.js
@@ -1,15 +1,30 @@
 // get the file system
 var fs = require("fs");
 
+// get the path
+var path = require("path");
+
+// get the first argument
+var dir = process.argv[2];
+
+// get the second argument
+var ext = process.argv[3];
+
 // read the passed parameter, fire function once it's read
-fs.readFile(process.argv[2], function finishedReading(error, userFile) {
+fs.readdir(dir, function finishedReading(error, list) {
     
     // throw an error if there is on
     if (error) return console.error(error)
     
-    // count the number of lines in the specified file
-    var numberOfLines = userFile.toString().split("\n").length - 1;
-
-    // return the number of specified lines
-    console.log(numberOfLines);
+    // loop through the list array
+    for (var i = 0; i < list.length; i++) {
+        
+        // set the current list item as the current file
+        var file = list[i];
+        
+        // if the path matches the specified extension, print the file name
+        if (path.extname(file) === "." + ext) {
+            console.log(file);
+        }
+    }
 })
